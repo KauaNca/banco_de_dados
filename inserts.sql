d8c9465694d33d2c6f90a12179d570d1c97453d1
Kauã Nunes <kauanunes2003@gmail.com>
2024-10-04 11:07:22 -0300

    atualizacao

---
+++ inserts.sql
@@ -0,0 +1,10 @@
+select * from pessoa;
+insert into pessoa(nome,cpf,idade,situacao) values ('Ana','0278888888',25,'A'),('Kauã','02222222222',21,'A'),('Cristina','5555555555',18,'D'),('Hudson','8888888888',15,'D');
+select * from professor;
+insert into professor(matricula,id_pessoa,situacao) values ('555555555',2,'A'),('888888888','1','D');
+select *from aluno;
+insert into aluno (id_pessoa,matricula,situacao)values(3,'8555555555','D'),(4,'5557998211','D');
+
+
+
+ 
